7c3aeb83a4e15c97c0010db84af06e4e4c8448bf
Salil Chincholikar <chincholikarsalil@gmail.com>
2023-04-26 09:02:10 +0530

    lambda and s3 Done: added s3 details to sync build folder using 'npm run sync'       used lambda to call the tvmaze api (currently using single function for all requests) Todo: use GET requests instead of POST (use query params instead of req body)

--- src/services/tvmaze.js
+++ src/services/tvmaze.js
@@ -1,43 +1,51 @@
 export const getSchedule = async () => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/schedule?country=US`,
-    {
-      crossDomain: true,
-    }
-  );
+  const response = await fetch(process.env.REACT_APP_AWS_LAMBDA_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Origin: "salil-tvmaze.s3-website.ap-south-1.amazonaws.com",
+    },
+    body: JSON.stringify({ name: "getSchedule" }),
+  });
   const schedule = await response.json();
   return schedule;
 };
 
 export const getShow = async (showId) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/shows/${showId}`,
-    {
-      crossDomain: true,
-    }
-  );
+  const response = await fetch(process.env.REACT_APP_AWS_LAMBDA_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Origin: "salil-tvmaze.s3-website.ap-south-1.amazonaws.com",
+    },
+    body: JSON.stringify({ name: "getShow", showId: showId }),
+  });
   const show = await response.json();
   return show;
 };
 
 export const getShowCast = async (showId) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/shows/${showId}/cast`,
-    {
-      crossDomain: true,
-    }
-  );
+  const response = await fetch(process.env.REACT_APP_AWS_LAMBDA_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Origin: "salil-tvmaze.s3-website.ap-south-1.amazonaws.com",
+    },
+    body: JSON.stringify({ name: "getShowCast", showId: showId }),
+  });
   const showCast = await response.json();
   return showCast;
 };
 
 export const getEpisode = async (episodeId) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/episodes/${episodeId}`,
-    {
-      crossDomain: true,
-    }
-  );
+  const response = await fetch(process.env.REACT_APP_AWS_LAMBDA_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Origin: "salil-tvmaze.s3-website.ap-south-1.amazonaws.com",
+    },
+    body: JSON.stringify({ name: "getEpisode", episodeId: episodeId }),
+  });
   const episode = await response.json();
   return episode;
 };
